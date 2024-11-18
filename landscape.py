e78d9e919c2d6cb515e9d7b685ddf4e09f5418a7
Emil Babayev <zentabit@gmail.com>
2024-11-18 13:55:38 +0100

    fix: rename some variables

--- landscape.py
+++ landscape.py
@@ -3,38 +3,39 @@ import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 
 ndim = 2
-nfunc = 3
+nfunc = 5
+peakedness = 100
 d = 1
 
-def plot2d():
-    x,y = np.mgrid[-1:1:.01, -1:1:.01]
-    pos = np.dstack((x,y))
+def plot2d(mus, covs):
+    x1,x2 = np.mgrid[0:d:.001, 0:d:.001]
+    pos = np.dstack((x1,x2))
 
-    z = f(pos)
+    y = f(pos, mus, covs)
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(x,y,z, vmin=z.min() * 2)
+    ax.plot_surface(x1,x2,y, vmin=y.min() * 2)
 
     plt.show()
 
 def generate_mv_gaussians(nfunc, ndim, d):
-    mus = 2 * d * np.random.rand(nfunc, ndim) - d
+    mus = d * np.random.rand(nfunc, ndim)
     covs = np.zeros((nfunc, ndim, ndim))
     for i in range(0, nfunc):
-        covs[i] = np.diag(0.1 + np.random.rand(ndim))
+        covs[i] = np.diag(0.05 + np.random.rand(ndim)/peakedness)
 
     return mus, covs
 
-def f(x):
-
-    mus, covs = generate_mv_gaussians(nfunc, ndim, d)
-
-    z2 = 0
+def f(x, mus, covs):
+    y = 0
     
     for vals in zip(mus, covs):
         rv = multivariate_normal(vals[0], vals[1])
-        z2 += rv.pdf(x)
+        y += rv.pdf(x)
 
-    return z2
+    return y
 
-plot2d()
+mus, covs = generate_mv_gaussians(nfunc, ndim, d)
+print(f((0.5,0.5), mus, covs))
+
+plot2d(mus, covs)
