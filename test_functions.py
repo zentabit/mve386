fd3fe6ed2f7e9b7a06b3808bb72fbb0f7c53dd33
Emil Babayev <emil@babayev.se>
2024-11-21 08:20:15 +0100

    feat: some nice test functions in 1d and 2d

---
+++ test_functions.py
@@ -0,0 +1,60 @@
+import landscape
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+
+def gauss1d(x):
+    mu = [ 0.5 ]
+    cov = [ 0.01 ]
+
+    return landscape.f_sca(x, mu, cov)
+
+def gauss2d(x,y): # two vectors as input
+    mus = [ 0.5, 0.5 ]
+    covs = np.diag([0.01, 0.01])
+
+    X, Y = np.meshgrid(x,y)
+    pos = np.dstack((X,Y))
+    rv = multivariate_normal(mus, covs)
+    return X,Y,rv.pdf(pos)
+
+def trough1d(x):
+    mus = [[0], [1]]
+    covs = [[0.1], [0.1]]
+
+    return landscape.f_sca(x, mus, covs)
+
+def trough2d(x,y):
+    mus = [
+        [1,0],
+        [1,1]
+    ]
+
+    covs = [
+        np.diag([0.1, 0.05]),
+        np.diag([0.1, 0.05])
+    ]
+
+    X, Y = np.meshgrid(x,y)
+    pos = np.dstack((X,Y))
+
+    return X, Y, landscape.f_sca(pos, mus, covs)
+
+def sheet1d(x, k = 10):
+    return 1/(1 + np.exp(-k * (x-0.5)))
+
+def sheet2d(x,y, k = 10):
+    X, Y = np.meshgrid(x,y)
+    pos = np.dstack((X,Y))
+
+    return X, Y, 1/(1 + np.exp(-k * (X-0.5)))
+
+# x = np.linspace(0, 1)
+# plt.plot(x, sheet1d(x))
+# plt.show()
+# X,Y,Z = sheet2d(x,x)
+
+# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+# ax.plot_surface(X,Y,Z, vmin=Z.min() * 2)
+
+# plt.show()
