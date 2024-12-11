f385675c5d4b15159530c6fedf1f878d5b0e9537
Emil Babayev <zentabit@gmail.com>
2024-12-11 14:42:40 +0100

    feat: refactor and 2d plotting

--- bench_plot.py
+++ bench_plot.py
@@ -1,19 +1,39 @@
 # This program takes a CSV file from the benchmarking scripts and plots it in linear-log scale.
 import numpy as np
 import matplotlib.pyplot as plt
+import json
+import sys
 
+def plot1d(vals, log, ax, fig):
+    ax.set_yscale('log')
+    ax.boxplot(vals[ :, 0, 0, 0, : ], tick_labels = log['batch_numbers'] )
+    ax.set_title("Hej")
+    ax.set_xlabel('N')
+    ax.set_ylabel('Relative entropy')
+    fig.tight_layout()
 
-timestamp = "1733906073.614063"
-fname = f"benchmark-{timestamp}.npy"
+def plot2d(vals, log, ax, fig):
+    vals_mean_func = np.mean(vals, axis = 0)
+    print(np.shape(vals_mean_func))
+    ax.imshow(vals_mean_func[: , 0, 0, : ], norm = 'log')
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.set_xticklabels(log['batch_numbers'])
+    fig.tight_layout()
 
-ax = plt.axes()
+def main():
+    fname = sys.argv[1]
+    vals = np.load(f"{fname}.npy")
+    with open(f"{fname}.json") as f:
+        log = json.load(f)
+    
+    print(log)
 
-vals = np.load(fname)
-print(vals)
-print([ str(label) for label in vals[:, 0] ])
-ax.set_yscale('log')
-ax.boxplot(vals[ :, 0, 0, 0, : ]) #, tick_labels =[ str(int(label)) for label in vals[:, 0] ] )
-ax.set_title("Hej")
-ax.set_xlabel('N')
-ax.set_ylabel('log(Relative entropy)')
-plt.show()
+    fig = plt.figure()
+    ax = plt.axes()
+    plot2d(vals, log, ax, fig)
+    plt.show()
+
+    
+
+main()
